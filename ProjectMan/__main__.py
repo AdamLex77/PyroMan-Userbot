7a7f15d37b3f939ce67648b5ab12a27c3398cb92
AdamLex77 <108127925+AdamLex77@users.noreply.github.com>
2022-07-10 09:55:07 +0800

    Update __main__.py

--- ProjectMan/__main__.py
+++ ProjectMan/__main__.py
@@ -1,11 +1,11 @@
 # Credits: @mrismanaziz
 # Copyright (C) 2022 Pyro-ManUserbot
 #
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# This file is a part of < https://github.com/adamlex77/PyroMan-Userbot/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/wibu_telegram & t.me/statusbotme
 
 from pyrogram import idle
 from uvloop import install
@@ -28,8 +28,8 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("Lunatic0de")
-            await bot.join_chat("SharingUserbot")
+            await bot.join_chat("statusbotme")
+            await bot.join_chat("wibu_telegram")
             await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
         except Exception as a:
             LOGGER("main").warning(a)
@@ -38,10 +38,10 @@ async def main():
 
 
 if __name__ == "__main__":
-    LOGGER("ProjectMan").info("Starting PyroMan-UserBot")
+    LOGGER("ProjectMan").info("Starting Pyrodam-UserBot")
     LOGGER("ProjectMan").info(f"Total Clients = {len(bots)} Users")
     install()
     git()
     heroku()
-    LOGGER("ProjectMan").info(f"PyroMan-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("ProjectMan").info(f"Pyrodam-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
     LOOP.run_until_complete(main())
